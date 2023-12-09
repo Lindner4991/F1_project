67e812a95b37661f56c5c501a21b2596d172a949
Lindner4991 <150828342+Lindner4991@users.noreply.github.com>
2023-12-09 16:44:28 +0100

    simulated data

--- simulated_data.R
+++ simulated_data.R
@@ -425,8 +425,141 @@ gamma <- seq(from = 0, to = 20, by = 1)
 
 
 
-# model 1 version 1 - 20% drv 80% ctr ####
-# placeholder
+# model 1 version 1 - 33.3% drv 66.7% ctr ####
+# number of constructors in time series
+K <- 12
+
+# number of drivers in time series
+N <- 24
+
+# number of qualifiers/races
+Q <- 159 # TODO first race
+
+# constructor qualifier/race NA indicators
+I_3 <- matrix(data = 1, nrow = K, ncol = Q)
+
+# NAs for constructor with ID 10 ( manor )
+I_3[10,c(16:18,59:159)] <- 0 # TODO first race
+
+# NAs for constructor with ID 11 ( caterham )
+I_3[11,16:159] <- 0 # TODO first race
+
+# NAs for constructor with ID 12 ( haas )
+I_3[12,1:37] <- 0 # TODO first race
+
+# constructor indicators
+I_2_1 <- matrix(data = NA, nrow = N, ncol = K)
+I_2_1[1,] <- c(1,0,0,0,0,0,0,0,0,0,0,0)
+I_2_1[2,] <- c(1,0,0,0,0,0,0,0,0,0,0,0)
+I_2_1[3,] <- c(0,1,0,0,0,0,0,0,0,0,0,0)
+I_2_1[4,] <- c(0,1,0,0,0,0,0,0,0,0,0,0)
+I_2_1[5,] <- c(0,0,1,0,0,0,0,0,0,0,0,0)
+I_2_1[6,] <- c(0,0,1,0,0,0,0,0,0,0,0,0)
+I_2_1[7,] <- c(0,0,0,1,0,0,0,0,0,0,0,0)
+I_2_1[8,] <- c(0,0,0,1,0,0,0,0,0,0,0,0)
+I_2_1[9,] <- c(0,0,0,0,1,0,0,0,0,0,0,0)
+I_2_1[10,] <- c(0,0,0,0,1,0,0,0,0,0,0,0)
+I_2_1[11,] <- c(0,0,0,0,0,1,0,0,0,0,0,0)
+I_2_1[12,] <- c(0,0,0,0,0,1,0,0,0,0,0,0)
+I_2_1[13,] <- c(0,0,0,0,0,0,1,0,0,0,0,0)
+I_2_1[14,] <- c(0,0,0,0,0,0,1,0,0,0,0,0)
+I_2_1[15,] <- c(0,0,0,0,0,0,0,1,0,0,0,0)
+I_2_1[16,] <- c(0,0,0,0,0,0,0,1,0,0,0,0)
+I_2_1[17,] <- c(0,0,0,0,0,0,0,0,1,0,0,0)
+I_2_1[18,] <- c(0,0,0,0,0,0,0,0,1,0,0,0)
+I_2_1[19,] <- c(0,0,0,0,0,0,0,0,0,1,0,0)
+I_2_1[20,] <- c(0,0,0,0,0,0,0,0,0,1,0,0)
+I_2_1[21,] <- c(0,0,0,0,0,0,0,0,0,0,1,0)
+I_2_1[22,] <- c(0,0,0,0,0,0,0,0,0,0,1,0)
+I_2_1[23,] <- c(0,0,0,0,0,0,0,0,0,0,0,1)
+I_2_1[24,] <- c(0,0,0,0,0,0,0,0,0,0,0,1)
+
+I_2_2 <- matrix(data = NA, nrow = N, ncol = K)
+I_2_2[1,] <- c(1,0,0,0,0,0,0,0,0,0,0,0)
+I_2_2[2,] <- c(0,0,0,0,0,0,0,0,1,0,0,0)  # switch
+I_2_2[3,] <- c(0,1,0,0,0,0,0,0,0,0,0,0)
+I_2_2[4,] <- c(0,1,0,0,0,0,0,0,0,0,0,0)
+I_2_2[5,] <- c(0,0,1,0,0,0,0,0,0,0,0,0)
+I_2_2[6,] <- c(0,0,1,0,0,0,0,0,0,0,0,0)
+I_2_2[7,] <- c(0,0,0,1,0,0,0,0,0,0,0,0)
+I_2_2[8,] <- c(0,0,0,1,0,0,0,0,0,0,0,0)
+I_2_2[9,] <- c(0,0,0,0,1,0,0,0,0,0,0,0)
+I_2_2[10,] <- c(0,0,0,0,1,0,0,0,0,0,0,0)
+I_2_2[11,] <- c(0,0,0,0,0,1,0,0,0,0,0,0)
+I_2_2[12,] <- c(0,0,0,0,0,1,0,0,0,0,0,0)
+I_2_2[13,] <- c(0,0,0,0,0,0,1,0,0,0,0,0)
+I_2_2[14,] <- c(0,0,0,0,0,0,1,0,0,0,0,0)
+I_2_2[15,] <- c(0,0,0,0,0,0,0,1,0,0,0,0)
+I_2_2[16,] <- c(0,0,0,0,0,0,0,1,0,0,0,0)
+I_2_2[17,] <- c(1,0,0,0,0,0,0,0,0,0,0,0)  # switch
+I_2_2[18,] <- c(0,0,0,0,0,0,0,0,1,0,0,0)
+I_2_2[19,] <- c(0,0,0,0,0,0,0,0,0,1,0,0)
+I_2_2[20,] <- c(0,0,0,0,0,0,0,0,0,1,0,0)
+I_2_2[21,] <- c(0,0,0,0,0,0,0,0,0,0,1,0)
+I_2_2[22,] <- c(0,0,0,0,0,0,0,0,0,0,1,0)
+I_2_2[23,] <- c(0,0,0,0,0,0,0,0,0,0,0,1)
+I_2_2[24,] <- c(0,0,0,0,0,0,0,0,0,0,0,1)
+
+I_2 <- list(I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,
+            I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,
+            I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,
+            I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,
+            I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,
+            I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,
+            I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,I_2_1,
+            I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,
+            I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,
+            I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,
+            I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,
+            I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,
+            I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,
+            I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,
+            I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,
+            I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2,I_2_2)  # TODO first race
+
+# driver qualifier/race NA indicators
+I_1 <- matrix(data = 1, nrow = N, ncol = Q)
+
+# NAs for driver with ID 19 ( manor driver )
+I_1[19,c(16:18,59:159)] <- 0 # TODO first race
+
+# NAs for driver with ID 20 ( manor driver )
+I_1[20,c(16:18,59:159)] <- 0 # TODO first race
+
+# NAs for driver with ID 21 ( caterham driver )
+I_1[21,16:159] <- 0 # TODO first race
+
+# NAs for driver with ID 22 ( caterham driver )
+I_1[22,16:159] <- 0 # TODO first race
+
+# NAs for constructor with ID 23 ( haas driver )
+I_1[23,1:37] <- 0 # TODO first race
+
+# NAs for constructor with ID 24 ( haas driver )
+I_1[24,1:37] <- 0 # TODO first race
+
+# number of ranks per qualifier/race
+J <- 22
+
+# initial conditions for latent constructor ability state equation
+mu_C_0 <- c(10+5, 9+5, 8+5, 7+5, 6+5, 5+5,
+            4+5, 3+5, 2+5, 1+5, 0+5, 1+5)  # increased by 5
+
+# initial conditions for latent driver ability state equation
+mu_D_0 <- c(10.25-5, 9.75-5, 9.25-5, 8.75-5, 8.25-5,
+            7.75-5, 7.25-5, 6.75-5, 6.25-5, 5.75-5,
+            5.25-5, 4.75-5, 4.25-5, 3.75-5, 3.25-5,
+            2.75-5, 2.25-5, 1.75-5, 1.25-5, 0.75-5,
+            0.25-5, -0.25-5, 0.75-5, 0.75-5)  # decreased by 5
+
+# SD for error for latent constructor ability state equations
+varsigma_C <- 0.4
+
+# SD for error for latent driver ability state equations
+varsigma_D <- 0.08
+
+# cut points
+gamma <- seq(from = 0, to = 20, by = 1)
 
 
 
@@ -452,10 +585,10 @@ fit_m1_v1_sim <- sampling(m1_v1_sim,
                           warmup = 0)
 
 # save fit_m1_v1_sim
-saveRDS(fit_m1_v1_sim, "data/fit_m1_v1_sim_greater_fluctuations.rds")
+saveRDS(fit_m1_v1_sim, "data/fit_m1_v1_sim_33drv_66ctr.rds")
 
 # load fit_m1_v1_sim
-fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_greater_fluctuations.rds")
+fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_33drv_66ctr.rds")
 
 
 # extract simulations
