b32c32a5858b0b69d7ea5666387db4d5ae6c4cc5
Lindner4991 <150828342+Lindner4991@users.noreply.github.com>
2023-12-11 11:19:54 +0100

    estimation

--- estimation.R
+++ estimation.R
@@ -7,14 +7,12 @@
 # this file builds on the simulated_data.R file
 
 
-# required data files ( occurrences in script marked with TODO data file ):
+# required data files:
 # fit_m___sim___.rds
-# R_act___.xlsx
-# drv_ctr___.xlsx
 # where ___ is a placeholder
 
 
-# required model files ( occurrences in script marked with TODO model file ):
+# required model files:
 # m___.stan
 # where ___ is a placeholder
 
@@ -62,7 +60,7 @@ gamma_lower <- 0
 gamma_upper <- J - 2
 
 # load fit_m1_v1_sim
-fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_clean_data.rds")  # TODO data file
+fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_clean_data.rds")
 # fit_m1_v1_sim <- readRDS("fit_m1_v1_sim_clean_data.rds")
 
 # extract simulations
@@ -81,7 +79,7 @@ gamma_lower <- 0
 gamma_upper <- J - 2
 
 # load fit_m1_v1_sim
-fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_missing_data.rds")  # TODO data file
+fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_missing_data.rds")
 # fit_m1_v1_sim <- readRDS("fit_m1_v1_sim_missing_data.rds")
 
 # extract simulations
@@ -103,7 +101,7 @@ gamma_lower <- 0
 gamma_upper <- J - 2
 
 # load fit_m1_v1_sim
-fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_greater_fluctuations.rds")  # TODO data file
+fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_greater_fluctuations.rds")
 # fit_m1_v1_sim <- readRDS("fit_m1_v1_sim_greater_fluctuations.rds")
 
 # extract simulations
@@ -119,13 +117,13 @@ R_sim <- R_sim_temp[40,,]
 
 
 
-# model 1 version 1 - 33.3% drv 66.7% ctr ####
+# model 1 version 1 - 25% drv 75% ctr ####
 # fixed cut points
 gamma_lower <- 0
 gamma_upper <- J - 2
 
 # load fit_m1_v1_sim
-fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_33drv_66ctr.rds")  # TODO data file
+fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_25drv_75ctr.rds")
 # fit_m1_v1_sim <- readRDS("fit_m1_v1_sim_missing_data.rds")
 
 # extract simulations
@@ -142,238 +140,6 @@ R_sim <- R_sim_temp[40,,]
 
 
 # model 1 version 1 - F1 hybrid era qualifier data ####
-# actual qualifier ranks
-# load R_act_temp
-R_act_temp <- read_excel("data/R_act_qualifier.xlsx",  # TODO data file
-                         sheet = "Sheet 1")
-# R_act_temp <- read_excel("R_act_qualifier.xlsx",  # TODO data file
-#                          sheet = "Sheet1")
-
-# data processing - delete columns 1, 2, 3, and 4
-R_act <- R_act_temp[,-c(1:4)]
-
-# data processing - chr --> numeric
-R_act <- sapply(R_act_temp, FUN = as.numeric)
-
-
-# number of constructors in time series
-K <- 12
-
-# number of drivers in time series
-N <- 51
-
-# number of qualifiers/races
-Q <- 159
-
-# constructor qualifier NA indicators
-I_3 <- matrix(data = 1, nrow = K, ncol = Q)
-
-# NAs for constructor with ID 10 ( manor )
-I_3[10,c(16:18,59:159)] <- 0
-
-# NAs for constructor with ID 11 ( caterham )
-I_3[11,c(16:17,19:159)] <- 0
-
-# NAs for constructor with ID 12 ( haas )
-I_3[12,1:37] <- 0
-
-# constructor indicators
-# load drv_ctr
-drv_ctr <- read_excel("data/drv_ctr_qualifier.xlsx",  # TODO data file
-                      sheet = "Sheet 1")
-# drv_ctr <- read_excel("drv_ctr_qualifier.xlsx",  # TODO data file
-#                       sheet = "Sheet 1")
-
-# test - same order of drv in drv_ctr and R_obs_qualifier
-test <- 0
-
-for (n in 1:N) {
-  
-  if (R_act_temp[n,1] == drv_ctr[n,1]) {
-    test <- test + 1
-  }
-    
-}
-
-test == N
-
-# data processing - delete column 1 and 2
-drv_ctr <- drv_ctr[,-c(1:2)]
-
-# data processing - constructor name changes
-for (n in 1:N) {
-  for (t in 1:Q) {
-    
-    if (!is.na(drv_ctr[n,t])) {
-      
-      if (drv_ctr[n,t] == "toro_rosso") {
-        drv_ctr[n,t] <- "alphatauri"
-      }
-      
-      if (drv_ctr[n,t] == "force_india" | drv_ctr[n,t] == "racing_point") {
-        drv_ctr[n,t] <- "aston_martin"
-      }
-      
-      if (drv_ctr[n,t] == "sauber") {
-        drv_ctr[n,t] <- "alfa"
-      }
-      
-      if (drv_ctr[n,t] == "marussia") {
-        drv_ctr[n,t] <- "manor"
-      }
-      
-      if (drv_ctr[n,t] == "lotus_f1" | drv_ctr[n,t] == "renault") {
-        drv_ctr[n,t] <- "alpine"
-      }
-      
-    }
-    
-  }
-}
-
-# data processing - replace NA for first qualifier with first non-NA value
-for (n in 1:N) {
-  
-  if (is.na(drv_ctr[n,1])) {
-    
-    t <- 2
-    success <- FALSE
-    
-    while (success == FALSE) {
-      
-      if (is.na(drv_ctr[n,t])) {
-        
-        t <- t + 1
-        
-      }
-      
-      else { success <- TRUE }
-      
-    }
-    
-    drv_ctr[n,1] <- drv_ctr[n,t]
-    
-  }
-  
-}
-
-# data processing - replace NA for qualifier t with t-1 value
-for (n in 1:N) {
-  for (t in 2:Q) {
-    
-    if (is.na(drv_ctr[n,t])) {
-      
-      drv_ctr[n,t] <- drv_ctr[n,t-1]
-      
-    }
-    
-  }
-}
-
-# data processing - constructor indicators for each qualifier
-I_2 <- list()
-
-for (t in 1:Q) {
-  
-  drv_ctr_extract <- drv_ctr[,t]
-  
-  I_2_temp <- matrix(data = NA, nrow = N, ncol = K)
-  
-  for (n in 1:N) {
-    
-    if (drv_ctr_extract[n] == "mercedes") {
-      I_2_temp[n,] <- c(1,0,0,0,0,0,0,0,0,0,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "red_bull") {
-      I_2_temp[n,] <- c(0,1,0,0,0,0,0,0,0,0,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "mclaren") {
-      I_2_temp[n,] <- c(0,0,1,0,0,0,0,0,0,0,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "ferrari") {
-      I_2_temp[n,] <- c(0,0,0,1,0,0,0,0,0,0,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "alphatauri") {
-      I_2_temp[n,] <- c(0,0,0,0,1,0,0,0,0,0,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "aston_martin") {
-      I_2_temp[n,] <- c(0,0,0,0,0,1,0,0,0,0,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "williams") {
-      I_2_temp[n,] <- c(0,0,0,0,0,0,1,0,0,0,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "alfa") {
-      I_2_temp[n,] <- c(0,0,0,0,0,0,0,1,0,0,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "alpine") {
-      I_2_temp[n,] <- c(0,0,0,0,0,0,0,0,1,0,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "manor") {
-      I_2_temp[n,] <- c(0,0,0,0,0,0,0,0,0,1,0,0)
-    }
-    
-    if (drv_ctr_extract[n] == "caterham") {
-      I_2_temp[n,] <- c(0,0,0,0,0,0,0,0,0,0,1,0)
-    }
-    
-    if (drv_ctr_extract[n] == "haas") {
-      I_2_temp[n,] <- c(0,0,0,0,0,0,0,0,0,0,0,1)
-    }
-    
-  }
-  
-  I_2[[t]] <- I_2_temp
-
-}
-
-# driver qualifier/race NA indicators
-I_1 <- matrix(data = 1, nrow = N, ncol = Q)
-
-for (n in 1:N) {
-  for (t in 1:Q) {
-    
-    if (is.na(R_act[n,t])) {
-      I_1[n,t] <- 0
-    }
-    
-  }
-}
-
-# actual qualifier ranks continued
-# data processing - NAs --> 22
-for (n in 1:N) {
-  for (t in 1:Q) {
-    
-    if (is.na(R_act[n,t])) {
-      R_act[n,t] <- 22
-    }
-    
-  }
-}
-
-# number of ranks per qualifier/race
-J <- 22
-
-# initial conditions for latent constructor ability state equation
-# placeholder
-
-# initial conditions for latent driver ability state equation
-# load drv_ctr_cockpit
-# ( derived from drv_ctr by manual data processing )
-drv_ctr_cockpit <- read_excel("data/drv_ctr_cockpit_qualifier.xlsx",
-                              sheet = "Sheet 1")
-
-# placeholder
-
 # fixed cut points
 gamma_lower <- 0
 gamma_upper <- J - 2
@@ -381,7 +147,55 @@ gamma_upper <- J - 2
 
 
 # model 1 version 1 - F1 hybrid era race data ####
-# placeholder
+# initial conditions for latent constructor ability state equation ( 75% )
+ctr_ability_rank <- c(15,14,13,12,11,10,9,8,7,6,5,5)
+
+mu_C_0 <- rep(NA, times = K)
+
+mu_C_0[1] <- ctr_ability_rank[2]  # 2013 ctr standings
+mu_C_0[2] <- ctr_ability_rank[1]  # 2013 ctr standings
+mu_C_0[3] <- ctr_ability_rank[5]  # 2013 ctr standings
+mu_C_0[4] <- ctr_ability_rank[3]  # 2013 ctr standings
+mu_C_0[5] <- ctr_ability_rank[8]  # 2013 ctr standings
+mu_C_0[6] <- ctr_ability_rank[6]  # 2013 ctr standings
+mu_C_0[7] <- ctr_ability_rank[9]  # 2013 ctr standings
+mu_C_0[8] <- ctr_ability_rank[7]  # 2013 ctr standings
+mu_C_0[9] <- ctr_ability_rank[4]  # 2013 ctr standings
+mu_C_0[10] <- ctr_ability_rank[10]  # 2013 ctr standings
+mu_C_0[11] <- ctr_ability_rank[11]  # 2013 ctr standings
+mu_C_0[12] <- ctr_ability_rank[11]  # min ctr_ability_rank ( haas )
+
+# initial conditions for latent driver ability state equation ( 25% )
+drv_ability_rank <- c(5.25,4.75,4.25,3.75,3.25,
+                      2.75,2.25,1.75,1.25,0.75,
+                      0.25,-0.25,-0.75,-1.25,-1.75,
+                      -2.25,-2.75,-3.25,-3.75,-4.25,
+                      -4.75 -5.25)
+
+mu_D_0 <- rep(NA, times = N)
+
+mu_D_0[1] <- drv_ability_rank[4]  # 2013 drv standings
+mu_D_0[2] <- drv_ability_rank[14]  # 2013 drv standings
+mu_D_0[3] <- drv_ability_rank[6]  # 2013 drv standings
+mu_D_0[4] <- drv_ability_rank[]
+mu_D_0[5] <- drv_ability_rank[2]  # 2013 drv standings
+mu_D_0[6] <- drv_ability_rank[15]  # 2013 drv standings
+mu_D_0[7] <- drv_ability_rank[10]  # 2013 drv standings
+mu_D_0[8] <- drv_ability_rank[]
+mu_D_0[9] <- drv_ability_rank[8]  # 2013 drv standings
+mu_D_0[10] <- drv_ability_rank[17]  # 2013 drv standings
+mu_D_0[11] <- drv_ability_rank[9]  # 2013 drv standings
+mu_D_0[12] <- drv_ability_rank[5]  # 2013 drv standings
+mu_D_0[13] <- drv_ability_rank[1]  # 2013 drv standings
+mu_D_0[14] <- drv_ability_rank[13]  # 2013 drv standings
+mu_D_0[15] <- drv_ability_rank[]
+mu_D_0[16] <- drv_ability_rank[11]  # 2013 drv standings
+mu_D_0[17] <- drv_ability_rank[22]  # 2013 drv standings
+mu_D_0[18] <- drv_ability_rank[19]  # 2013 drv standings
+mu_D_0[19] <- drv_ability_rank[16]  # 2013 drv standings
+mu_D_0[20] <- drv_ability_rank[]
+mu_D_0[21] <- drv_ability_rank[7]  # 2013 drv standings
+mu_D_0[22] <- drv_ability_rank[18]  # 2013 drv standings
 
 
 
@@ -408,7 +222,7 @@ fit_m1_v1 <- sampling(m1_v1,
                       iter = iter_per_chain)
 
 # save fit_m1_v1
-saveRDS(fit_m1_v1, "results/fit_m1_v1_33drv_66ctr.rds")
+saveRDS(fit_m1_v1, "results/fit_m1_v1_25drv_75ctr.rds")
 
 
 
@@ -437,7 +251,7 @@ job::job({
                         iter = iter_per_chain)
   
   # save fit_m1_v1
-  saveRDS(fit_m1_v1, "fit_m1_v1_33drv_66ctr.rds")
+  saveRDS(fit_m1_v1, "fit_m1_v1_25drv_75ctr.rds")
   
 })
 
