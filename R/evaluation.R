d3e1cb4476e30925b41fcdaea015406531fa116d
Lindner4991 <150828342+Lindner4991@users.noreply.github.com>
2023-12-17 17:57:38 +0100

    evaluation

--- R/evaluation.R
+++ R/evaluation.R
@@ -154,7 +154,11 @@ HDI_PCACC <- function(pred, obs, X, Y, I, iter) {
 
 
 # Spearman’s rank correlation coefficient for matrix
-# placeholder
+rho <- function(pred, obs, X, Y, I) {
+  
+  # placeholder
+  
+}
 
 
 # 89% HDI for Spearman’s rank correlation coefficient for matrix
