0bfbb857cf47f74bfb26334602dd293f3499d784
Lindner4991 <150828342+Lindner4991@users.noreply.github.com>
2023-12-30 19:13:40 +0100

    estimation

--- R/estimation.R
+++ R/estimation.R
@@ -84,9 +84,6 @@ params_m1_v1_sim <- rstan::extract(fit_m1_v1_sim)
 # extract simulated qualifier/race ranks
 R_sim_temp <- params_m1_v1_sim$R_sim
 
-# extract simulated qualifier/race ranks
-R_sim_temp <- params_m1_v1_sim$R_sim
-
 R_sim <- R_sim_temp[40,,]
 
 
@@ -105,9 +102,6 @@ params_m1_v1_sim <- rstan::extract(fit_m1_v1_sim)
 # extract simulated qualifier/race ranks
 R_sim_temp <- params_m1_v1_sim$R_sim
 
-# extract simulated qualifier/race ranks
-R_sim_temp <- params_m1_v1_sim$R_sim
-
 R_sim <- R_sim_temp[40,,]
 
 
@@ -122,9 +116,6 @@ fit_m1_v1_sim <- readRDS("data/fit_m1_v1_sim_dominant_ctr_ability.rds")
 
 # extract simulations
 params_m1_v1_sim <- rstan::extract(fit_m1_v1_sim)
-
-# extract simulated qualifier/race ranks
-R_sim_temp <- params_m1_v1_sim$R_sim
 
 # extract simulated qualifier/race ranks
 R_sim_temp <- params_m1_v1_sim$R_sim
@@ -242,9 +233,6 @@ params_m1_v2_sim <- rstan::extract(fit_m1_v2_sim)
 # extract simulated qualifier/race ranks
 R_sim_temp <- params_m1_v2_sim$R_sim
 
-# extract simulated qualifier/race ranks
-R_sim_temp <- params_m1_v2_sim$R_sim
-
 R_sim <- R_sim_temp[40,,]
 
 
@@ -259,9 +247,6 @@ fit_m1_v2_sim <- readRDS("data/fit_m1_v2_sim_increased_fluctuations.rds")
 
 # extract simulations
 params_m1_v2_sim <- rstan::extract(fit_m1_v2_sim)
-
-# extract simulated qualifier/race ranks
-R_sim_temp <- params_m1_v2_sim$R_sim
 
 # extract simulated qualifier/race ranks
 R_sim_temp <- params_m1_v2_sim$R_sim
@@ -284,9 +269,6 @@ params_m1_v2_sim <- rstan::extract(fit_m1_v2_sim)
 # extract simulated qualifier/race ranks
 R_sim_temp <- params_m1_v2_sim$R_sim
 
-# extract simulated qualifier/race ranks
-R_sim_temp <- params_m1_v2_sim$R_sim
-
 R_sim <- R_sim_temp[40,,]
 
 
@@ -294,7 +276,6 @@ R_sim <- R_sim_temp[40,,]
 # model 1 version 2 - F1 hybrid era qualifier data ####
 # SD increase for error for latent driver ability state equation
 # in case of driver change
-# first try
 kappa_D <- 0.5
 
 # fixed cut points
@@ -307,7 +288,6 @@ gamma_upper <- J - 2
 # initial conditions for latent constructor ability state equation ( 75% )
 # SD increase for error for latent driver ability state equation
 # in case of driver change
-# first try
 kappa_D <- 0.5
 
 # fixed cut points
@@ -377,6 +357,169 @@ job::job({
   # save fit_m1_v2
   saveRDS(fit_m1_v2,
           "fit_m1_v2_clean_data.rds")  # TODO adjust name
+  
+})
+
+
+
+# model 2 - simulated data - clean data ####
+# fixed cut points
+gamma_lower <- 0
+gamma_upper <- J - 2
+
+# load fit_m2_sim
+fit_m2_sim <- readRDS("data/fit_m2_sim_clean_data.rds")
+
+# extract simulations
+params_m2_sim <- rstan::extract(fit_m2_sim)
+
+# extract simulated qualifier/race ranks
+R_sim_temp <- params_m2_sim$R_sim
+
+R_sim <- R_sim_temp[40,,]
+
+
+
+# model 2 - simulated data - missing data ####
+# fixed cut points
+gamma_lower <- 0
+gamma_upper <- J - 2
+
+# load fit_m2_sim
+fit_m2_sim <- readRDS("data/fit_m2_sim_missing_data.rds")
+
+# extract simulations
+params_m2_sim <- rstan::extract(fit_m2_sim)
+
+# extract simulated qualifier/race ranks
+R_sim_temp <- params_m2_sim$R_sim
+
+R_sim <- R_sim_temp[40,,]
+
+
+
+# model 2 - increased fluctuations ####
+# fixed cut points
+gamma_lower <- 0
+gamma_upper <- J - 2
+
+# load fit_m2_sim
+fit_m2_sim <- readRDS("data/fit_m2_sim_increased_fluctuations.rds")
+
+# extract simulations
+params_m2_sim <- rstan::extract(fit_m2_sim)
+
+# extract simulated qualifier/race ranks
+R_sim_temp <- params_m2_sim$R_sim
+
+R_sim <- R_sim_temp[40,,]
+
+
+
+# model 2 - dominant ctr ability ####
+# fixed cut points
+gamma_lower <- 0
+gamma_upper <- J - 2
+
+# load fit_m2_sim
+fit_m1_v2_sim <- readRDS("data/fit_m2_sim_dominant_ctr_ability.rds")
+
+# extract simulations
+params_m2_sim <- rstan::extract(fit_m2_sim)
+
+# extract simulated qualifier/race ranks
+R_sim_temp <- params_m2_sim$R_sim
+
+R_sim <- R_sim_temp[40,,]
+
+
+
+# model 2 - F1 hybrid era qualifier data ####
+# SD increase for error for latent driver ability state equation
+# in case of driver change
+kappa_D <- 0.5
+
+# fixed cut points
+gamma_lower <- 0
+gamma_upper <- J - 2
+
+
+
+# model 2 - F1 hybrid era race data ####
+# initial conditions for latent constructor ability state equation ( 75% )
+# SD increase for error for latent driver ability state equation
+# in case of driver change
+kappa_D <- 0.5
+
+# fixed cut points
+gamma_lower <- 0
+gamma_upper <- J - 2
+
+
+
+
+
+# model 2 - estimation - without job ####
+# computation with NUTS in STAN
+m2 <- stan_model("STAN/m2.stan")
+
+# number of sampling iterations per chain
+iter_per_chain <- 2000
+
+fit_m2 <- sampling(m2,
+                   data = list(K = K,
+                               N = N,
+                               T = Q,
+                               I_3 = I_3,
+                               I_2 = I_2,
+                               I_1 = I_1,
+                               I_4 = I_4,
+                               J = J,
+                               mu_C_0 = mu_C_0,
+                               mu_D_0 = mu_D_0,
+                               kappa_C = kappa_C,
+                               kappa_D = kappa_D,
+                               gamma_lower = gamma_lower,
+                               gamma_upper = gamma_upper,
+                               R = R_sim),  # TODO actual data
+                   iter = iter_per_chain)
+
+# save fit_m2
+saveRDS(fit_m2,
+        "results/fit_m2_clean_data.rds")  # TODO adjust name
+
+
+
+# model 2 - estimation - with job ####
+# computation with NUTS in STAN
+m2 <- stan_model("m2.stan")
+
+# number of sampling iterations per chain
+iter_per_chain <- 2000
+
+job::job({
+  
+  fit_m2 <- sampling(m2,
+                     data = list(K = K,
+                                 N = N,
+                                 T = Q,
+                                 I_3 = I_3,
+                                 I_2 = I_2,
+                                 I_1 = I_1,
+                                 I_4 = I_4,
+                                 J = J,
+                                 mu_C_0 = mu_C_0,
+                                 mu_D_0 = mu_D_0,
+                                 kappa_C = kappa_C,
+                                 kappa_D = kappa_D,
+                                 gamma_lower = gamma_lower,
+                                 gamma_upper = gamma_upper,
+                                 R = R_sim),  # TODO actual data
+                     iter = iter_per_chain)
+  
+  # save fit_m2
+  saveRDS(fit_m2,
+          "fit_m2_clean_data.rds")  # TODO adjust name
   
 })
 
